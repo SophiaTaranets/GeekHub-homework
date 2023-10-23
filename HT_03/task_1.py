c611017f79065aec41ec09140412c786825acb61
SophiaTaranets <sony.taranets@gmail.com>
2023-10-23 11:58:05 +0300

    changes

--- HT_03/task_1.py
+++ HT_03/task_1.py
@@ -2,49 +2,15 @@
 # The list of tuples can be hardcoded. The "replacement" value is entered by user.
 # The number of elements in the tuples must be different.
 
-# Tuples for the test
-# t = (2023, 'geekhub', True, 12)
-# t = (2023, 'geekhub', True, 12, 15.5)
-# t = (2023, 'geekhub', True, 12, 15.5, [1, 2, 3])
-# t = (2023, 'geekhub', True, 12, 15.5, (1, 2, 3))
-# t = (2023, 'geekhub', True, 12, 15.5, {1: 1})
-t = (2023, 'geekhub', True, 12, 15.5, [1, 2, 3], 'string')
-lst = list(t)
-
-print('Some rules to use the program:\n'
-      '1 - You can replace elements only with similar type\n'
-      '2 - If type of the last elements is the collection like '
-      'list, tuple or set you should enter elements with separator " "\n'
-      '3 - If type of the last elements is "dict" you should enter elements this format:'
-      'key1:value1,key2:value2,')
-print(f'Consider the type of last value in current tuple is {type(lst[-1])}')
-
-new_value = input('Enter last value: ')
-try:
-    if isinstance(lst[-1], bool):
-        lst[-1] = bool(new_value)
-    elif isinstance(lst[-1], int):
-        lst[-1] = int(new_value)
-    elif isinstance(lst[-1], float):
-        lst[-1] = float(new_value)
-    elif isinstance(lst[-1], str):
-        lst[-1] = new_value
-    elif isinstance(lst[-1], list):
-        lst[-1] = new_value.split(' ')
-    elif isinstance(lst[-1], tuple):
-        lst[-1] = tuple(new_value.split(' '))
-    elif isinstance(lst[-1], set):
-        lst[-1] = set(new_value.split(' '))
-    elif isinstance(lst[-1], dict):
-        d = {}
-        s = new_value.split(',')
-        for i in s:
-            k = i.split(':')
-            d[k[0]] = k[1]
-        lst[-1] = d
+list_of_tuples = [(), (99, ), (1, 2, 3), (1, 2, 3, 4)]
+result = []
+value = int(input('Enter value: '))
+for t in list_of_tuples:
+    if len(t) == 0:
+        result.append(t)
     else:
-        last_value = new_value
-except Exception as e:
-    print(e)
-
-print(f'Changed tuple: {tuple(lst)}')
+        t = list(t)
+        last_index = len(t) - 1
+        t[last_index] = value
+        result.append(tuple(t))
+print(result)
--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -1,25 +1,31 @@
 # Write a script to get the maximum and minimum value in a dictionary.
 
-def value_len(v):
-    try:
-        iter(v)
-        return len(v)
-    except TypeError:
-        return v
+def find_min_max_strings(dict_data: dict):
+    group_strings = [value for value in dict_data.values() if isinstance(value, str)]
+    return f'Between Strings: min={min(group_strings)} max={max(group_strings)}'
 
 
-def get_key(d: dict, v):
-    for k in d:
-        if d[k] == v:
-            return k
+def find_min_max_numbers(dict_data: dict):
+    group_numbers = [value for value in dict_data.values() if isinstance(value, (int, float))]
+    return f'Between Numbers: min={min(group_numbers)} max={max(group_numbers)}'
 
 
-test_dict = {1: 100, 2: 10, 3: 110, 4: 6, 5: 'string', 6: [1, 2, 4], 7: (1, 2, 4)}
-d_copy = test_dict.copy()
-for key, value in test_dict.items():
-    d_copy[key] = value_len(value)
+def find_min_max_collections(dict_data: dict):
+    group_collections = [value for value in dict_data.values() if isinstance(value, (list, tuple, set))]
+    min_collection = min(group_collections, key=lambda x: min(x))
+    max_collection = max(group_collections, key=lambda x: max(x))
+    return f'Between Collections: min={min_collection} max={max_collection}'
 
-print(test_dict)
-lst_values = list(d_copy.values())
-print(f'Minimum value in the dictionary: {get_key(d_copy, min(lst_values))}:{min(lst_values)}')
-print(f'Maximum value in the dictionary: {get_key(d_copy, max(lst_values))}:{max(lst_values)}')
+
+test_dict = {1: 100, 2: 10,
+             3: 110.4,
+             4: 6,
+             5: 'string',
+             6: [1, 2, 4],
+             7: (1, 2, 4, 12),
+             8: 'geekhub',
+             9: [10, 20, 40]}
+
+print(find_min_max_strings(test_dict))
+print(find_min_max_numbers(test_dict))
+print(find_min_max_collections(test_dict))
