be42141e33035b765b5a59969faf73e6d637611a
SophiaTaranets <sony.taranets@gmail.com>
2023-10-23 12:53:02 +0300

    changes

--- HT_03/task_1.py
+++ HT_03/task_1.py
@@ -3,14 +3,15 @@
 # The number of elements in the tuples must be different.
 
 list_of_tuples = [(), (99, ), (1, 2, 3), (1, 2, 3, 4)]
-result = []
-value = int(input('Enter value: '))
-for t in list_of_tuples:
-    if len(t) == 0:
-        result.append(t)
+result_list = []
+new_last_value = int(input('Enter new value for the last element: '))
+for list_element in list_of_tuples:
+    if list_element:
+        list_element = list(list_element)
+        last_index = len(list_element) - 1
+        list_element[last_index] = new_last_value
+        result_list.append(tuple(list_element))
     else:
-        t = list(t)
-        last_index = len(t) - 1
-        t[last_index] = value
-        result.append(tuple(t))
-print(result)
+        result_list.append(list_element)
+
+print(f'Result: {result_list}')
