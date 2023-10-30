b399d5168a5112ae2e71b8caa47196c81e72d9ed
SophiaTaranets <sony.taranets@gmail.com>
2023-10-30 13:15:50 +0200

    changes

--- HT_05/task_5.py
+++ HT_05/task_5.py
@@ -3,29 +3,32 @@
 # Операції що мають бути присутні: +, -, *, /, %, //, **. Не забудьте протестувати з різними значеннями на предмет помилок!
 
 def calculator(first_number: float, second_number: float, operation: str):
-    if operation in ['+', '-', '*', '/', '%', '//', '**']:
-        if operation == '+':
-            return first_number + second_number
-        elif operation == '-':
-            return first_number - second_number
-        elif operation == '*':
-            return first_number * second_number
-        elif operation == '/':
-            if second_number == 0:
-                return 'Error: Division by zero is not allowed.'
-            else:
-                return first_number / second_number
-        elif operation == '%':
-            return first_number % second_number
-        elif operation == '//':
-            if second_number == 0:
-                return 'Error: Division by zero is not allowed.'
-            else:
-                return int(first_number // second_number)
-        elif operation == '**':
-            return first_number ** second_number
-    else:
-        return f'Invalid operation. Supported operators are +, -, *, /, %, //, **.'
+    try:
+        if operation in ['+', '-', '*', '/', '%', '//', '**']:
+            if operation == '+':
+                return first_number + second_number
+            elif operation == '-':
+                return first_number - second_number
+            elif operation == '*':
+                return first_number * second_number
+            elif operation == '/':
+                if second_number == 0:
+                    return 'Error: Division by zero is not allowed.'
+                else:
+                    return first_number / second_number
+            elif operation == '%':
+                return first_number % second_number
+            elif operation == '//':
+                if second_number == 0:
+                    return 'Error: Division by zero is not allowed.'
+                else:
+                    return int(first_number // second_number)
+            elif operation == '**':
+                return first_number ** second_number
+        else:
+            return f'Invalid operation. Supported operators are +, -, *, /, %, //, **.'
+    except Exception as e:
+        return e
 
 
 try:
