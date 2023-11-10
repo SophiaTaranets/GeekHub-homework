dea2270b56db9d03e0b03dec702463df684d8519
SophiaTaranets <sony.taranets@gmail.com>
2023-11-10 15:56:43 +0200

    changes

--- HT_08/task_2.py
+++ HT_08/task_2.py
@@ -1,9 +1,5 @@
 # Запишіть в один рядок генератор списку (числа в діапазоні від 0 до 100), сума цифр кожного елемент якого буде дорівнювати 10.
 #    Результат: [19, 28, 37, 46, 55, 64, 73, 82, 91]
 
-def digits_sum(number: int):
-    return sum([int(digit) for digit in str(number)])
-
-
-list_generator = [i for i in range(0, 100) if digits_sum(i) == 10]
+list_generator = [i for i in range(0, 100) if sum([int(j) for j in str(i)]) == 10]
 print(f'Result: {list_generator}')
